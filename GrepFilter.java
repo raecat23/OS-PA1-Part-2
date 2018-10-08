73530962aa1011b7fa78f4d64eb4bdac0c5210ef
raecat23 <43554898+raecat23@users.noreply.github.com>
2018-10-08 14:09:52 -0400

    Fixed isDone and RR

---
+++ GrepFilter.java
@@ -0,0 +1,41 @@
+package cs131.pa1.filter.concurrent;
+
+import cs131.pa1.filter.Message;
+
+public class GrepFilter extends ConcurrentFilter {
+	private String toFind;
+	private boolean isDone;
+	public GrepFilter(String line) throws Exception {
+		super();
+		String[] param = line.split(" ");
+		if(param.length > 1) {
+			toFind = param[1];
+		} else {
+			System.out.printf(Message.REQUIRES_PARAMETER.toString(), line);
+			throw new Exception();
+		}
+	}
+	
+	public void process(){
+		while (!input.isEmpty() && isDone() == false){
+			String line = input.poll();
+			String processedLine = processLine(line);
+			if (processedLine != null){
+				output.add(processedLine);
+			}
+		}
+		
+		ConcurrentREPL.moveProcess(this);
+	}
+	public String processLine(String line) {
+		
+		if(line.contains(toFind)) {
+				return line;
+			} else {
+				return null;
+		}
+	}
+	public boolean isDone(){
+		return isDone;
+	}
+}
---
+++ WcFilter.java
@@ -0,0 +1,53 @@
+package cs131.pa1.filter.concurrent;
+
+public class WcFilter extends ConcurrentFilter {
+	private int linecount;
+	private int wordcount;
+	private int charcount;
+	
+	public WcFilter() {
+		super();
+	}
+	
+	public void process() {
+		if(isDone()) {
+			output.add(processLine(null));
+		} else {
+			if(!input.isEmpty()){
+				String line = input.poll();
+				String processedLine = processLine(line);
+				if (processedLine != null){
+					output.add(processedLine);
+				}
+			}	
+		}
+		ConcurrentREPL.moveProcess(this);
+	}
+	
+	public String processLine(String line) {
+		//prints current result if ever passed a null
+		if(line == null) {
+			return linecount + " " + wordcount + " " + charcount;
+		}
+		
+		if(isDone()) {
+			String[] wct = line.split(" ");
+			wordcount += wct.length;
+			String[] cct = line.split("|");
+			charcount += cct.length;
+			return ++linecount + " " + wordcount + " " + charcount;
+		} else {
+			linecount++;
+			String[] wct = line.split(" ");
+			wordcount += wct.length;
+			String[] cct = line.split("|");
+			charcount += cct.length;
+			if(!isDone()){
+				//move to the next process
+			}
+			return null;
+		}
+		
+	}
+	
+}
